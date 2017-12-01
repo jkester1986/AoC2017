fd363e4902efb286798b2c153303faf8d9fad613
Julia Kester <kester@oswego.edu>
2017-12-01 01:08:49 -0500

    Day 1 solved

--- Day1.js
+++ Day1.js
@@ -1,8 +1,23 @@
 function parseInput(){
 	var text = document.getElementById("text").value;
 	//var lines = text.split('\n');
-	
-	for(var i = 0; i < text.length; i++){
+
+	let total = 0;
+	let total2 = 0;
+	let length = text.length;
+	let halfLength = length/2;
+
+	text = text.split('').map(Number);
+
+	for(var i = 0; i < length; i++) {
+		if(text[i] === text[(i+1)%length]) {
+			total += text[i];
+		}
+		if(text[i] === text[(i+halfLength)%length]) {
+			total2 += text[i];
+		}
 	}
-	console.log("the answer");
+
+	document.getElementById("answer1").innerHTML = `Answer1: ${total}`;
+	document.getElementById("answer2").innerHTML = `Answer2: ${total2}`;
 }
