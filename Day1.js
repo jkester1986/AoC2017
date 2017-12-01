b7c2d29944bbb3653b081222eed200dbe649d35c
Julia Kester <kester@oswego.edu>
2017-11-30 19:55:11 -0500

    Day 1 prep

---
+++ Day1.js
@@ -0,0 +1,8 @@
+function parseInput(){
+	var text = document.getElementById("text").value;
+	//var lines = text.split('\n');
+	
+	for(var i = 0; i < text.length; i++){
+	}
+	console.log("the answer");
+}
