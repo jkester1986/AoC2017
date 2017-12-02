490ed109cc0ad8b563ef4a7f9b5b147457a53753
Julia Kester <kester@oswego.edu>
2017-12-02 00:35:41 -0500

    fixed tab/space mixups

--- Day2.js
+++ Day2.js
@@ -1,28 +1,26 @@
 function parseInput(){
-	var text = document.getElementById("text").value;
-	var lines = text.split('\n');
+	let text = document.getElementById("text").value;
+	let lines = text.split('\n');
+	let length = lines.length;
 
-  let length = lines.length;
-
-  let total = 0;
-  let total2 = 0;
+	let total = 0;
+	let total2 = 0;
 
 	for(var i = 0; i < length; i++) {
-    window.console.log(lines[i].split('\t'));
-    let nums = lines[i].split('\t').map(Number);
-    let lowest = 1000000000000000;
-    let highest = 0;
-    for (var j = 0; j < 16; j++ ) {
-      let num = nums[j];
-      if (num < lowest) lowest = num;
-      if (num > highest) highest = num;
-      for (var k = 0; k < 16; k++ ) {
-        if (num % nums[k] === 0 && j !== k) total2 += num/nums[k];
-      }
-    }
-    window.console.log(`high: ${highest}`);
-    console.log(`low: ${lowest}`);
-    total += highest - lowest;
+		let nums = lines[i].split('\t').map(Number);
+		let lowest = 1000000000000000;
+		let highest = 0;
+
+		for (var j = 0; j < 16; j++ ) {
+			let num = nums[j];
+			if (num < lowest) lowest = num;
+			if (num > highest) highest = num;
+			for (var k = 0; k < 16; k++ ) {
+				if (num % nums[k] === 0 && j !== k) total2 += num/nums[k];
+			}
+		}
+
+		total += highest - lowest;
 	}
 
 	document.getElementById("answer1").innerHTML = `Answer1: ${total}`;
