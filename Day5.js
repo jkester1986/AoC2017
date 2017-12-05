7bad34422066145a9180ce0a318a1a393c938e9f
Julia Kester <julia.kester@synacor.com>
2017-12-05 16:09:33 -0500

    Day 5 solved

--- Day5.js
+++ Day5.js
@@ -3,20 +3,32 @@ fs.readFile('Day5.txt', 'utf8', function(err, data) {
 	if(err) {
 		return console.log(err);
 	}
-	let text = data
+	let text = data;
 	let lines = text.split('\n').map(Number);
+	lines = lines.splice(0, lines.length-1);
 	let length = lines.length;
-	lines = lines.splice(0, length-1);
-	let steps = 0;
-	let index = 0;
+
+	let steps = 0, index = 0, offset = 0;
 
 	let end = false;
 
 	while (!end) {
 		let origIndex = index;
-		index += lines[index];
-		lines[origIndex]++;
-		if(isNaN(index)) break;
+		offset = lines[index];
+		index += offset
+
+		if(isNaN(index)) {
+			console.log("NaN");
+			break;
+		}
+
+		if (offset >= 3) {
+			lines[origIndex]--;
+		}
+		else {
+			lines[origIndex]++;
+		}
+
 		steps++;
 		if(index >= length) {
 			end = true;
