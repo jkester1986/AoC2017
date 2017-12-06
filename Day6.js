f539c7625223000c126a28752eda6201349f8f9a
Julia Kester <julia.kester@synacor.com>
2017-12-06 14:26:48 -0500

    Day 6 solved

---
+++ Day6.js
@@ -0,0 +1,32 @@
+let banks = [4,10,4,1,8,4,9,14,5,1,14,15,0,15,3,5];
+//let banks = [0,2,7,0];
+let match = false;
+let bankOrderings = new Array();
+let steps = 0;
+
+bankOrderings.push(banks.toString());
+
+while (!match) {
+  let max = Math.max(...banks);
+  let index = banks.indexOf(max);
+  banks[index] = 0;
+  index++;
+  while (max > 0) {
+    banks[index%16]++;
+    index++;
+    max--;
+  }
+  steps++;
+
+  //console.log(banks);
+
+  if(bankOrderings.includes(banks.toString())) match = true;
+  else bankOrderings.push(banks.toString());
+}
+
+let banksMatch = banks.toString();
+
+let matchIndex = bankOrderings.indexOf(banksMatch);
+
+console.log(`Part 1 Answer: ${steps}`);
+console.log(`Part 2 Answer: ${steps-matchIndex}`);
