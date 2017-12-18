b1838acba8b261dd6900f8f7ba1bdd68345dfdea
Julia Kester <julia.kester@synacor.com>
2017-12-18 18:06:15 -0500

    Day 18 partial

--- Day18.js
+++ Day18.js
@@ -5,36 +5,67 @@ fs.readFile('Day18.txt', 'utf8', function(err, data) {
 	}
 	data = data.trim();
 	let directions = data.split('\n');
+	let length = directions.length;
 	let index = 0;
 	let nonzero = false;
+	let register = new Set();
+	let sound = null;
+	let count = 0;
+
+	function getVal(match) {
+		let matchInt = parseInt(match);
+		if(isNaN(matchInt)) {
+			if (register[`${match}`])
+				return register[`${match}`];
+			else
+				return 0;
+		}
+		else return matchInt;
+	}
 
 	while(!nonzero) {
-		let match = directions[i].match(/^(\w+)\s(\w)(\s(.+))?$/)
-		index++;
+		let match = directions[index].match(/^(\w+)\s(\w)(\s(.+))?$/);
+
+		switch(match[1]) {
+			case 'set':
+				register[`${match[2]}`] = getVal(match[4]);
+					parseInt(match[4]);
+				index++;
+				break;
+			case 'add':
+				register[`${match[2]}`] = getVal(match[2]) + getVal(match[4]);
+				index++;
+				break;
+			case 'mul':
+				register[`${match[2]}`] *= getVal(match[2]) + getVal(match[4]);
+				index++;
+				break;
+			case 'mod':
+				register[`${match[2]}`] %= getVal(match[2]) + getVal(match[4]);
+				index++;
+				break;
+			case 'snd':
+				sound = getVal(match[2]);
+				index++;
+				break;
+			case 'rcv':
+				if(sound && getVal(match[2]) !== 0) {
+					nonzero = true;
+				}
+				index++;
+				break;
+			case 'jgz':
+				if(getVal(match[2]) > 0) {
+					index += getVal(match[4]);
+				}
+				index++
+				break;
+		}
+
+		index %= length;
+		count++;
 	}
-
-	switch(match[1]) {
-		case 'set':
-
-			break;
-		case 'add':
-
-			break;
-		case 'mul':
-
-			break;
-		case 'mod':
-
-			break;
-		case 'snd':
-
-			break;
-		case 'rcv':
-
-			break;
-		case 'jgz':
-
-			break;
-	}
+	console.log(sound);
+	console.log(count);
 
 });
