717dfa795c97ad9b38dc5775653f654ad4007275
Julia Kester <kester@oswego.edu>
2017-12-18 21:33:18 -0500

    Day 18 struggling on p2

--- Day18.js
+++ Day18.js
@@ -6,66 +6,138 @@ fs.readFile('Day18.txt', 'utf8', function(err, data) {
 	data = data.trim();
 	let directions = data.split('\n');
 	let length = directions.length;
-	let index = 0;
-	let nonzero = false;
-	let register = new Set();
+	let index1 = 0;
+	let index2 = 0;
+	let deadlock = false;
+	let toggle = true;
+	let register1 = new Set();
+	let register2 = new Set();
+	let queue1 = [];
+	let queue2 = [];
 	let sound = null;
+	let sent = 0;
 	let count = 0;
 
 	function getVal(match) {
 		let matchInt = parseInt(match);
 		if(isNaN(matchInt)) {
-			if (register[`${match}`])
-				return register[`${match}`];
-			else
-				return 0;
+			if(toggle) {
+				if (register1[`${match}`])
+					return register1[`${match}`];
+				else
+					return 0;
+			}
+			else {
+				if (register2[`${match}`])
+					return register2[`${match}`];
+				else
+					return 1;
+			}
 		}
 		else return matchInt;
 	}
 
-	while(!nonzero) {
-		let match = directions[index].match(/^(\w+)\s(\w)(\s(.+))?$/);
+	while(!deadlock) {
+		let pattern = /^(\w+)\s(\w)(\s(.+))?$/;
+		let match = toggle ? directions[index1].match(pattern) : directions[index2].match(pattern);
 
 		switch(match[1]) {
 			case 'set':
-				register[`${match[2]}`] = getVal(match[4]);
-					parseInt(match[4]);
-				index++;
+				if(toggle) {
+					register1[`${match[2]}`] = getVal(match[4]);
+					index1++;
+				}
+				else {
+					register2[`${match[2]}`] = getVal(match[4]);
+					index2++;
+				}
 				break;
 			case 'add':
-				register[`${match[2]}`] = getVal(match[2]) + getVal(match[4]);
-				index++;
+				if(toggle) {
+					register1[`${match[2]}`] = getVal(match[2]) + getVal(match[4]);
+					index1++;
+				}
+				else {
+					register2[`${match[2]}`] = getVal(match[2]) + getVal(match[4]);
+					index2++;
+				}
 				break;
 			case 'mul':
-				register[`${match[2]}`] *= getVal(match[2]) + getVal(match[4]);
-				index++;
+				if(toggle) {
+					register1[`${match[2]}`] = getVal(match[2]) * getVal(match[4]);
+					index1++;
+				}
+				else {
+					register2[`${match[2]}`] = getVal(match[2]) * getVal(match[4]);
+					index2++;
+				}
 				break;
 			case 'mod':
-				register[`${match[2]}`] %= getVal(match[2]) + getVal(match[4]);
-				index++;
+				if(toggle) {
+					register1[`${match[2]}`] = getVal(match[2]) % getVal(match[4]);
+					index1++;
+				}
+				else {
+					register2[`${match[2]}`] = getVal(match[2]) % getVal(match[4]);
+					index2++;
+				}
 				break;
 			case 'snd':
-				sound = getVal(match[2]);
-				index++;
+				if(toggle) {
+					queue2.push(getVal(match[2]));
+					sent++;
+					index1++;
+				}
+				else {
+					queue1.push(getVal(match[2]));
+					index2++;
+				}
 				break;
 			case 'rcv':
-				if(sound && getVal(match[2]) !== 0) {
-					nonzero = true;
+				if(toggle) {
+					if(queue1[0] !== undefined ) {
+						register1[`${match[2]}`] = queue1[0];
+						queue1.length > 1 ? queue1.shift() : queue1 = [];
+						index1++;
+					}
+					else {
+						if(queue1[0] === undefined && queue2[0] === undefined) deadlock = true;
+						toggle = false;
+					}
 				}
-				index++;
+				else {
+					if(queue2[0] !== undefined) {
+						register2[`${match[2]}`] = queue2[0];
+						queue2.length > 1 ? queue2.shift() : queue2 = [];
+						index2++;
+					}
+					else {
+						if(queue1[0] === undefined && queue2[0] === undefined) deadlock = true;
+						toggle = true;
+					}
+				}
 				break;
 			case 'jgz':
-				if(getVal(match[2]) > 0) {
-					index += getVal(match[4]);
+				if(toggle) {
+					if(getVal(match[2]) > 0) {
+						index1 += getVal(match[4]);
+					}
+					else index1++;
 				}
-				index++
+				else {
+					if(getVal(match[2]) > 0) {
+						index2 += getVal(match[4]);
+					}
+					else index2++;
+				}
 				break;
 		}
 
-		index %= length;
+		index1 %= length;
+		index2 %= length;
 		count++;
 	}
-	console.log(sound);
-	console.log(count);
+	//console.log(sound);
+	console.log(sent);
 
 });
