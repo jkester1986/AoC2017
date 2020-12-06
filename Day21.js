f12b27f4d9c41836826a7ad78e273d2cf461be94
Julia Kester <kester@oswego.edu>
2020-12-05 19:14:07 -0500

    Day 21: better setup

--- Day21.js
+++ Day21.js
@@ -4,43 +4,79 @@ fs.readFile('Day21.txt', 'utf8', function(err, data) {
 		return console.log(err);
 	}
 	data = data.trim();
-	let directions = data.split('\n'),
-		twoByTwo = {},
-		threeByThree = {},
-		count = 0,
+	let lines = data.split('\n'),
+		directions = {},
 		grid = [['.','.','#'], ['#','.','#'], ['.','#','#']];
 
-	directions.forEach(function(dir) {
+	// put directions in a dictionary
+	lines.forEach(dir => {
 		let match = dir.match(/^(.+)\s\=>\s(.+)$/);
-		if(count < 6) {
-			twoByTwo[`${match[1]}`] = match[2];
-		}
-		else threeByThree[`${match[1]}`] = match[2];
-		count++;
+		let input = match[1],
+			output = match[2];
+
+		directions[`${input}`] = output;
 	});
 
-	for(var y = 0; y < grid.length; y++) {
-		let string = '';
-		for(var x = 0; x < grid[0].length; x++) {
-			string += grid[x][y];
-		}
-		console.log(string);
-	}
-
-	let count = 0;
-	while(count < 5) {
-		let divisor = grid.length%2 ? 2 : 3,
-			squares = grid.length/divisor,
-			square = 0;
-			count = 0;
-			string = ''
-		while(square < squares) {
-			for(var y = 0; y < divisor; y++) {
-				for(var x = 0;  x < divisor; x++);
-			}
-			count += divisor;
-			square++;
+	// iterate
+	let limit = 0;
+	while(limit < 0) {
+		if(grid.length % 2 === 0) {
+			//TODO:
 		}
 	}
-
 });
+
+
+function flipX(input) {
+	let split = input.split("/");
+	if(split.length == 2) {
+		let bottom = split[1];
+		split[1] = split[0];
+		split[0] = bottom;
+		return `${split[0]}/${split[1]}`;
+	}
+	else {
+		let bottom = split[2];
+		split[2] = split[0];
+		split[0] = bottom;
+		return `${split[0]}/${split[1]}/${split[2]}`;
+	}
+}
+
+function flipY(input) {
+	let split = input.split("/");
+	let reversals = [];
+	split.forEach(string => {
+		reversals.push(reverse(string));
+	});
+
+	return reversals.join("/");
+}
+
+function rotate(input) {
+	let rows = input.split('/');
+	if(rows.length === 2) {
+		rows[0] = rows[0].charAt(1) + rows[1].charAt(1);
+		rows[1] = rows[1].charAt(0) + rows[0].charAt(0);
+	}
+	else {
+		rows[0] = rows[0].charAt(1) + rows[0].charAt(2) + rows[1].charAt(2);
+		rows[1] = rows[0].charAt(0) + rows[1].charAt(1) + rows[2].charAt(2);
+		rows[2] = rows[1].charAt(0) + rows[2].charAt(0) + rows[2].charAt(1);
+	}
+
+	return rows.join("/");
+}
+
+//keeping it simple, since our lenghth is only ever 2 or 3
+function reverse(string) {
+	let chars = string.split("");
+	let lengthIndex = chars.length-1;
+
+	let swap = chars[0];
+	chars[0] = chars[lengthIndex];
+	chars[lengthIndex] = swap;
+	let joined = chars.join('');
+
+	return joined;
+}
